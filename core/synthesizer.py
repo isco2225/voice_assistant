3ea0c29b7bc7dcb6fa3352ef33dfbb77f1301b9c
isco2225 <omran.haj20@gmail.com>
2025-05-24 21:19:30 +0300

    Feat: the speaker changed to google speaker.

--- core/synthesizer.py
+++ core/synthesizer.py
@@ -1,9 +1,24 @@
-import pyttsx3
-
-engine = pyttsx3.init()
-engine.setProperty('rate', 150)
-engine.setProperty('volume', 0.9)
+from gtts import gTTS
+import playsound
+import os
+import uuid
 
 def speak(text):
-    engine.say(text)
-    engine.runAndWait()
+    filename = f"temp_{uuid.uuid4()}.mp3"
+    tts = gTTS(text=text, lang='tr')
+    tts.save(filename)
+    playsound.playsound(filename)
+    os.remove(filename)
+
+
+
+
+#import pyttsx3
+
+#engine = pyttsx3.init()
+#engine.setProperty('rate', 150)
+#engine.setProperty('volume', 0.9)
+
+#def speak(text):
+#    engine.say(text)
+#    engine.runAndWait()
