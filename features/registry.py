dec48e67025d3b451f50585d47b7a9dbb80553ea
isco2225 <omran.haj20@gmail.com>
2025-05-25 21:20:18 +0300

    Refect: imports are on one file now.

---
+++ features/registry.py
@@ -0,0 +1,21 @@
+# facts
+from features.facts.facts_controller import give_fact
+
+# apps / webs
+from features.apps.open_app import open_application
+from features.webs.open_website import open_website
+
+# time
+from features.time_and_history.time_query import handle_time_command, handle_date_command
+
+# modes
+from features.modes.developer_mode import open_developer_mode
+
+__all__ = [
+    "give_fact",
+    "open_application",
+    "open_website",
+    "handle_time_command",
+    "handle_date_command",
+    "open_developer_mode",
+]
